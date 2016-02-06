4b4585329284916d56099f56c0b4ead443bdb03c
Jake Seeber <zippyjake1@gmail.com>
2016-02-06 11:54:59 -0500

    save

---
+++ www/js/play.js
@@ -0,0 +1,22 @@
+//State: DogeDodge
+
+'use strict'
+
+var DogeDodge = {};
+
+DogeDodge.Play = function () {};
+
+DogeDodge.Play.prototype = {
+  init: function () {
+    console.log("%c~~~ Booting the Game ~~~\n Made by Jake S")
+  },
+
+  preload: function () {
+  },
+
+  create: function () {
+  },
+
+  update: function () {
+  }
+};
