db49430c2901a9723010fbefb78d3089c249ce57
Jake Seeber <zippyjake1@gmail.com>
2016-02-13 12:01:15 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -8,13 +8,17 @@ DogeDodge.Play = function () {};
 
 DogeDodge.Play.prototype = {
   init: function () {
-    console.log("%c~~~ Booting the Game ~~~\n Made by Jake S")
+    console.log("%c~~~ Booting the Game ~~~\n Made by Jake S","color:#00ff00;");
   },
 
   preload: function () {
+    this.load.image('background','assets/background.jpg');
   },
 
   create: function () {
+    this.background = this.add.tileSprite(0,0,320,568,'background');
+    this.background.autoScroll(50,-50);
+    this.background.scale.set(1);
   },
 
   update: function () {
